5ab891a290729ab5978954e3b8ff18f8950789d7
Hristo Tanchev <h.tanchev@emerchantpay.com>
2013-12-28 21:20:24 +0200

    to_json

--- app/controllers/uiss_info_controller.rb
+++ app/controllers/uiss_info_controller.rb
@@ -27,7 +27,7 @@ class UissInfoController < ApplicationController
     #end
 
     respond_to do |format|
-      format.json { render json: $marks_array }
+      format.json { render json: $marks_array.to_json }
       format.html { render action: 'show' }
       
     end
